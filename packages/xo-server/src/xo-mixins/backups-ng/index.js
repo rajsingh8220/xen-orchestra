acb2bf883fd1e2545c11a0d7c22c568da7083419
Badr <azizbibadr@gmail.com>
2018-04-24 14:58:39 +0200

    add "reportWhen" to the backupNg job's settings

--- packages/xo-server/src/xo-mixins/backups-ng/index.js
+++ packages/xo-server/src/xo-mixins/backups-ng/index.js
@@ -43,11 +43,16 @@ import { translateLegacyJob } from './migration'
 
 type Mode = 'full' | 'delta'
 
+type GlobalSettings = {|
+  reportWhen?: string,
+|}
+
 type Settings = {|
   deleteFirst?: boolean,
   exportRetention?: number,
   snapshotRetention?: number,
   vmTimeout?: number,
+  ''?: GlobalSettings,
 |}
 
 type SimpleIdPattern = {|
@@ -139,6 +144,7 @@ const getOldEntries = <T>(retention: number, entries?: T[]): T[] =>
 const defaultSettings: Settings = {
   deleteFirst: false,
   exportRetention: 0,
+  reportWhen: 'failure',
   snapshotRetention: 0,
   vmTimeout: 0,
 }
